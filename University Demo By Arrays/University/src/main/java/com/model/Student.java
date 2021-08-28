d36801fb59a18969abb55a22f8836eb931543eed
Devallanagendra <87415451+Devallanagendra@users.noreply.github.com>
2021-08-28 13:16:36 +0530

    Add files via upload
    UniversityDemo by Arrays Implementation of One to Many Relation

---
+++ University Demo By Arrays/University/src/main/java/com/model/Student.java
@@ -0,0 +1,55 @@
+package com.model;
+
+public class Student {
+	private int studId;
+	private String studName;
+	private int studAge;
+	private Department department;
+
+	
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int studId, String studName, int studAge) {
+		super();
+		this.studId = studId;
+		this.studName = studName;
+		this.studAge = studAge;
+	}
+
+	public int getStudId() {
+		return studId;
+	}
+
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
+
+	public String getStudName() {
+		return studName;
+	}
+
+	public void setStuName(String studName) {
+		this.studName = studName;
+	}
+
+	public int getStudAge() {
+		return studAge;
+	}
+
+	public void setStudAge(int studAge) {
+		this.studAge = studAge;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+}
